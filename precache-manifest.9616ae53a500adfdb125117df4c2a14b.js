c4f961880bb9c2a5ecaaa1a0ac2c33f9eee65b23
Luis Avila <x4288177@gmail.com>
2020-03-10 09:19:54 -0400

    Updates

---
+++ precache-manifest.9616ae53a500adfdb125117df4c2a14b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94dff8f08af0968df81246cae487450f",
+    "url": "/sangria71/index.html"
+  },
+  {
+    "revision": "61214f9eadfc2bf17efb",
+    "url": "/sangria71/static/css/main.76e242e9.chunk.css"
+  },
+  {
+    "revision": "aa3b0580349c639175da",
+    "url": "/sangria71/static/js/2.cefe3883.chunk.js"
+  },
+  {
+    "revision": "4d8c58c763eb6f656d38d13317751fe9",
+    "url": "/sangria71/static/js/2.cefe3883.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61214f9eadfc2bf17efb",
+    "url": "/sangria71/static/js/main.27e2a6e6.chunk.js"
+  },
+  {
+    "revision": "adaa067a634d6c9aa43a",
+    "url": "/sangria71/static/js/runtime-main.43b1b00f.js"
+  }
+]);
